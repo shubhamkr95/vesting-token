627ce1fc55ef890f0142c59eca2d0db64197b35d
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-20 10:18:03 +0530

    added API key & wallet private key to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,16 @@ require("dotenv").config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const ALCHEMY_API_KEY = process.env.API_KEY;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 module.exports = {
  solidity: "0.8.0",
+ networks: {
+  rinkeby: {
+   url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+   accounts: [`${RINKEBY_PRIVATE_KEY}`],
+  },
+ },
 };
