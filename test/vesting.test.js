9e896d57e2f260a7a86f1348ec14be69718263d7
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-21 20:19:57 +0530

    checking vesting details

--- test/vesting.test.js
+++ test/vesting.test.js
@@ -38,4 +38,36 @@ describe("Token Vesting", function () {
    );
   });
  });
+
+ describe("Vesting smart contract", function () {
+  it("Should create vesting ", async function () {
+   // create vesting
+   const roles = [0, 1, 2]; // Advisor (0), Partnerships(1), Mentors(2)
+   const createVesting = await vesting.createVesting(
+    roles[0],
+    advisor.address,
+    0,
+    1682081183 //UNIX time - Fri Apr 21 2023
+   );
+   await createVesting.wait();
+   expect(await vesting.isVested(advisor.address)).to.equal(true);
+  });
+
+  it("Should release vesting", async function () {
+   // Approve vesting contract
+   const tokenTotalSupply = await token.totalSupply();
+   const parseTokenTotalSupply = tokenTotalSupply.toString();
+   const approveContract = await token.approve(
+    vesting.address,
+    ethers.BigNumber.from(parseTokenTotalSupply)
+   );
+   approveContract.wait();
+   expect(await token.allowance(admin.address, vesting.address)).to.equal(
+    ethers.BigNumber.from(parseTokenTotalSupply)
+   );
+  });
+
+  // Create vesting
+  const roles = [0, 1, 2]; // Advisor (0), Partnerships(1), Mentors(2)
+ });
 });
